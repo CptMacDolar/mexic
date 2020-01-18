c9341d5db77fcfbf4d5a2972f108dfd171190612
Maciek <macdolar167@gmail.com>
2020-01-18 19:02:10 +0100

    v0.75 update
    -Dying
    -Respawn

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -10,19 +10,28 @@ public class Control : MonoBehaviour
     public float speedY;
 
     public Button jump_button;
+    public Button die_button;
     public VariableJoystick variableJoystick;
     public Rigidbody2D rb;
+    public Sprite dead;
+   // public SpriteRenderer spriteRenderer;
 
     public bool isControlActive;
+    public bool kill;
 
     void Start()
     {
+        kill = false;
         isControlActive = true;
-        jump_button.onClick.AddListener(TaskOnClick);
+        jump_button.onClick.AddListener(Jump);
+        die_button.onClick.AddListener(Die);
     }
 
     void Update()
     {
+        if (kill && spriteRenderer.sprite != dead)
+            changeSprite();
+
         if (!isControlActive)
             return;
 
@@ -31,7 +40,13 @@ public class Control : MonoBehaviour
         GetComponent<Rigidbody2D>().velocity = velocity;
     }
 
-    private void TaskOnClick()
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Salt")
+            kill = true;
+    }
+
+    private void Jump()
     {
         if (!isControlActive)
             return;
@@ -40,5 +55,17 @@ public class Control : MonoBehaviour
         velocity.y = speedY;
         GetComponent<Rigidbody2D>().velocity = velocity;
     }
+    private void Die()
+    {
+        if (!isControlActive)
+            return;
+        kill = true;
+    }
+
+    public void changeSprite()
+    {
+        spriteRenderer.sprite = dead;
+    }
+
 
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Control[] gameObjects;
+
+    public Button jump_button;
+    public Button die_button;
+    public VariableJoystick variableJoystick;
+    public Sprite dead;
+    public SpriteRenderer spriteRenderer;
+    int index = 0;
+
+    private Control currentControl;
+
+    private void Awake()
+    {
+        Spawn();
+    }
+
+    private void Update()
+    {
+        if (currentControl.kill)
+            CharacterDied();
+    }
+
+    private void Spawn()
+    {
+        if(index < 3)
+        {
+            currentControl = Instantiate(gameObjects[index]);
+            currentControl.jump_button = jump_button;
+            currentControl.die_button = die_button;
+            currentControl.variableJoystick = variableJoystick;
+            currentControl.dead = dead;
+            //currentControl.spriteRenderer = gameObjects[index].spriteRenderer;
+    index++;
+        }
+    }
+
+    public void CharacterDied()
+    {
+        currentControl.rb.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        currentControl.isControlActive = false;
+        Spawn();
+    }
+}
